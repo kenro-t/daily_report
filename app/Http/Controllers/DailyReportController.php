9328cf5566c1c730ece111d21cbb0a7a43b685e7
kenro-t <ngkn1425@gmail.com>
2023-07-27 23:47:14 +0900

    createメソッドの作成

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\DailyReport;
+// use Illuminate\Support\Facades\Auth;
 
 class DailyReportController extends Controller
 {
@@ -12,5 +13,17 @@ class DailyReportController extends Controller
         $daily_reports = DailyReport::with('dailyReportDetails')->get();
         // dd($daily_reports);
         return view('daily_report.index', ['daily_reports' => $daily_reports]);
+    }
+
+    // create
+    public function create($date) {
+        // dd(\Auth::user()->id);
+        $dailyReport = DailyReport::with('dailyReportDetails')
+            ->where('posted_date', $date)
+            ->where('user_id',\Auth::user()->id)
+            ->first();
+            
+        dd($dailyReport);
+        return view('daily_report.create',['daily_report' => $dailyReport]);
     }
 }
--- resources/views/daily_report/index.blade.php
+++ resources/views/daily_report/index.blade.php
@@ -21,13 +21,13 @@
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
 
 <script>
-    let target = `{{ route('daily_report.edit',['date'=>'target']) }}`;
+    let target = `{{ route('daily_report.create',['date'=>'targetDate']) }}`;
     document.addEventListener('DOMContentLoaded', ()=> {
         const calendarEl = document.getElementById('calendar');
         const calendar = new FullCalendar.Calendar(calendarEl, {
             initialView: 'dayGridMonth',
             dateClick: (e)=> {
-                target = target.replace(/(target)/g,e.dateStr)
+                target = target.replace('targetDate',e.dateStr)
                 window.location.href = target;
             }
         });
