6dad8f1dc9972cddf1595604c6633b491d04ce85
kenro-t <ngkn1425@gmail.com>
2023-07-31 22:25:48 +0900

    新規日報画面のコメント追加

--- resources/views/daily_report/create.blade.php
+++ resources/views/daily_report/create.blade.php
@@ -6,8 +6,8 @@
 
 <form action="{{ route('daily_report.store', ['date' => $date]) }}" method="post">
     @csrf
-    {{-- old('name')でwithInputの値が取れる --}}
-    案件名<input type="text" name='project_title' value="{{ old('project_title') }}"><br>
+    {{-- old('name')でwithInputの値が取れる 第２引数を設定することでoldの値が存在しない場合の値をセットできる --}}
+    案件名<input type="text" name='project_title' value="{{ old('project_title', 'デフォルト名') }}"><br>
     内容<textarea name="detail" id="" cols="30" rows="10">{{ old('detail') }}</textarea><br>
     <input type="submit" value='提出する'>
 </form>
