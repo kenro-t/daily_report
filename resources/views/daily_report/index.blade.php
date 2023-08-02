e1f8f49735792831793537e421e014df3b8dece0
kenro-t <ngkn1425@gmail.com>
2023-08-03 01:20:20 +0900

    ヘッダーフッターを切り出して管理

--- resources/views/daily_report/index.blade.php
+++ resources/views/daily_report/index.blade.php
@@ -1,54 +1,68 @@
+
 @extends('layouts.parent')
 
-<h1>日報カレンダー一覧</h1>
+@section('head')
+    @component('layouts.head')
+    @endcomponent
+@endsection
+
+@section('header')
+    @component('layouts.header')
+    @endcomponent
+@endsection
 
 @section('content')
-<!-- FullCalenderの導入　後でMixに移行する -->
-<div id='calendar' style='max-width:800px; margin:0 auto;'></div>
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
+    {{-- <h1>日報カレンダー一覧</h1> --}}
+    <!-- FullCalenderの導入　後でMixに移行する -->
+    <div id='calendar' style='max-width:800px; margin:0 auto;'></div>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
 
-<script>
-    // 日報データをjsonで渡す
-    const json_daily_reports = @json($daily_reports);
-    const daily_reports = JSON.parse(json_daily_reports);
-    const events = [];
-    // 日報データの数だけループ
-    for (const daily_report of daily_reports) {
-        // その日に登録されたプロジェクトの数だけループ
-        for (const daily_report_detail of daily_report.daily_report_details) {
-            events.push({
-                id: daily_report.id,// ユニークID
-                start: daily_report.posted_date,// イベント開始日
-                end: "",// イベント終了日
-                title: daily_report_detail.project_title,// イベントのタイトル
-                description: daily_report_detail.detail,// イベントの詳細
-                backgroundColor: "red",// 背景色
-                borderColor: "red",// 枠線色
-                editable: true// イベント操作の可否
-            })
-        };
+    <script>
+        // 日報データをjsonで渡す
+        const json_daily_reports = @json($daily_reports);
+        const daily_reports = JSON.parse(json_daily_reports);
+        const events = [];
+        // 日報データの数だけループ
+        for (const daily_report of daily_reports) {
+            // その日に登録されたプロジェクトの数だけループ
+            for (const daily_report_detail of daily_report.daily_report_details) {
+                events.push({
+                    id: daily_report.id,// ユニークID
+                    start: daily_report.posted_date,// イベント開始日
+                    end: "",// イベント終了日
+                    title: daily_report_detail.project_title,// イベントのタイトル
+                    description: daily_report_detail.detail,// イベントの詳細
+                    backgroundColor: "red",// 背景色
+                    borderColor: "red",// 枠線色
+                    editable: true// イベント操作の可否
+                })
+            };
 
-    }
+        }
 
-    let create_target = `{{ route('daily_report.create',['date'=>'targetDate']) }}`;
-    let edit_target = `{{ route('daily_report.edit',['date'=>'targetDate']) }}`;
+        let create_target = `{{ route('daily_report.create',['date'=>'targetDate']) }}`;
+        let edit_target = `{{ route('daily_report.edit',['date'=>'targetDate']) }}`;
 
-    document.addEventListener('DOMContentLoaded', ()=> {
-        const calendarEl = document.getElementById('calendar');
-        const calendar = new FullCalendar.Calendar(calendarEl, {
-            initialView: 'dayGridMonth',
-            events:events,
-            dateClick: (e)=> {
-                const changedTarget = create_target.replace('targetDate',e.dateStr)
-                window.location.href = changedTarget;
-            },
-            eventClick: (e)=>{// イベントのクリックイベント
-		        const changedTarget = edit_target.replace('targetDate',e.event.startStr)
-                window.location.href = changedTarget;
-            }
+        document.addEventListener('DOMContentLoaded', ()=> {
+            const calendarEl = document.getElementById('calendar');
+            const calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                events:events,
+                dateClick: (e)=> {
+                    const changedTarget = create_target.replace('targetDate',e.dateStr)
+                    window.location.href = changedTarget;
+                },
+                eventClick: (e)=>{// イベントのクリックイベント
+                    const changedTarget = edit_target.replace('targetDate',e.event.startStr)
+                    window.location.href = changedTarget;
+                }
+            });
+            calendar.render();
         });
-        calendar.render();
-    });
-</script>
+    </script>  
+@endsection
 
+@section('footer')
+    @component('layouts.footer')
+    @endcomponent
 @endsection
--- resources/views/layouts/parent.blade.php
+++ resources/views/layouts/parent.blade.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html> --}}
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="UTF-8">
-    <title>日報カレンダー</title>
-    <meta name="description" content="日報カレンダー">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name') }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.5.0/dist/full.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.tailwindcss.com"></script>
+    @yield('head')
 </head>
     
 <body>
-<div class="container">
-    @yield('content')
-</div>
-</body>
+    <header>
+        @yield('header')
+    </header>
+    <div class="container">
+        @yield('content')
+    </div>
+    </body>
+    <footer>
+        @yield('footer')
+    </footer>
 </html>
