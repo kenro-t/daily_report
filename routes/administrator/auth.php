115a093bb496878494a4607623b04627fceac394
kenro-t <ngkn1425@gmail.com>
2023-08-16 23:48:13 +0900

    administratorのrouteの記法と指定を修正

--- routes/administrator/auth.php
+++ routes/administrator/auth.php
@@ -11,41 +11,32 @@ use App\Http\Controllers\Administrator\Auth\RegisteredUserController;
 use App\Http\Controllers\Administrator\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest:administrator')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
-                ->middleware('guest:administrator')
                 ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])
-                ->middleware('guest:administrator');
+    Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->middleware('guest:administrator')
                 ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])
-                ->middleware('guest:administrator');
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-                ->middleware('guest:administrator')
                 ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-                ->middleware('guest:administrator')
                 ->name('password.email');
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-                ->middleware('guest:administrator')
                 ->name('password.reset');
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
-                ->middleware('guest:administrator')
                 ->name('password.store');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth:administrator')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
-                ->middleware('auth:administrator')
                 ->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
@@ -57,17 +48,13 @@ Route::middleware('auth')->group(function () {
                 ->name('verification.send');
 
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->middleware('auth:administrator')
                 ->name('password.confirm');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store'])
-                ->middleware('auth:administrator');
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
     Route::put('password', [PasswordController::class, 'update'])
-                ->middleware('auth:administrator')
                 ->name('password.update');
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth:administrator')
                 ->name('logout');
 });
