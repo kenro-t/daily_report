11c2c981f4fe26d7a3101e93f0087538879a90ea
kenro-t <ngkn1425@gmail.com>
2023-08-24 23:44:54 +0900

    戻るボタン追加

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,7 @@
 <footer>
-    フッター
+    @if (Request::routeIs('daily_report.edit', 'daily_report.create'))
+        <div>
+            <a class="btn" id="back" href="{{ route('daily_report.index') }}">戻る</a>
+        </div>
+    @endif
 </footer>
