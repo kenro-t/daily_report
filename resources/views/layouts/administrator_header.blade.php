b245eea4037eb8b71de9c6deab93dd3439893c4b
kenro-t <ngkn1425@gmail.com>
2023-09-01 22:53:50 +0900

    ログアウト、ホームボタンのリンク先設定

--- resources/views/layouts/administrator_header.blade.php
+++ resources/views/layouts/administrator_header.blade.php
@@ -4,9 +4,14 @@
             <h1 class="text-xl font-semibold text-gray-800">日報システムダッシュボード</h1>
             <nav>
                 <ul class="flex space-x-4">
-                    <li><a href="#" class="text-gray-600 hover:text-gray-800">ホーム</a></li>
-                    <li><a href="#" class="text-gray-600 hover:text-gray-800">設定</a></li>
-                    <li><a href="#" class="text-gray-600 hover:text-gray-800">ログアウト</a></li>
+                    <li><a href="{{ route('administrator.index') }}" class="text-gray-600 hover:text-gray-800">ホーム</a></li>
+                    {{-- <li><a href="#" class="text-gray-600 hover:text-gray-800">設定</a></li> --}}
+                    <li>
+                        <form action="{{ route('administrator.logout') }}" class="mb-0" method="post">
+                            @csrf
+                            <input type="submit" class="text-gray-600 hover:text-gray-800" value="ログアウト">
+                        </form>
+                    </li>
                 </ul>
             </nav>
         </div>
